14a476c9dd04f458250bcad4318d22130e863aa0
Roman Kostetskyi <roman.kostetskyi@renenergyglobal.com>
2025-07-08 18:58:36 +0300

    refactor: improve type assertions in FeaturesDecorator

--- projects/common/src/lib/feature.decorator.ts
+++ projects/common/src/lib/feature.decorator.ts
@@ -1,23 +1,23 @@
-import { ɵComponentType, ɵDirectiveType, Type } from "@angular/core";
-import { ComponentFeature, ComponentFeatures } from "./component.feature";
-import { DirectiveFeature, DirectiveFeatures } from "./directive.feature";
+import { ɵComponentType, ɵDirectiveType, Type } from '@angular/core';
+import { ComponentFeature, ComponentFeatures } from './component.feature';
+import { DirectiveFeature, DirectiveFeatures } from './directive.feature';
 
 type Feature = DirectiveFeature | ComponentFeature;
 
 function FeaturesDecorator(features: Feature[]) {
   return <T>(componentType: Type<T>) => {
     Promise.resolve().then(() => {
-      const def: any = componentType as ɵDirectiveType<T> & ɵComponentType<T>; // TODO: fix any
+      const def = componentType as ɵDirectiveType<T> & ɵComponentType<T>;
 
       if (!def.ɵcmp && !def.ɵdir) {
-        throw new Error("Ivy is not enabled.");
+        throw new Error('Ivy is not enabled.');
       }
 
       if (def.ɵcmp) {
-        ComponentFeatures(def, features, def.ɵfac);
+        ComponentFeatures(def, features, def.ɵfac as () => T);
       }
       if (def.ɵdir) {
-        DirectiveFeatures(def, features, def.ɵfac);
+        DirectiveFeatures(def, features, def.ɵfac as () => T);
       }
     });
   };
